e036a555c0b1a216b20562c1ef72e693608e2805
Raisul Islam <138724286+Raisul447@users.noreply.github.com>
2025-09-24 15:36:04 +0600

    settings.php

--- settings.php
+++ settings.php
@@ -6,8 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action('admin_menu', 'sdwc_register_settings_page');
 function sdwc_register_settings_page() {
     add_options_page(
-        esc_html__( 'WC Review Captcha Settings', 'spam-defender-wooreview-captcha' ),
-        esc_html__( 'WC Review Captcha', 'spam-defender-wooreview-captcha' ),
+        esc_html__( 'WC Review Captcha Settings', 'spam-defender-review-captcha-for-woocommerce' ),
+        esc_html__( 'WC Review Captcha', 'spam-defender-review-captcha-for-woocommerce' ),
         'manage_options',
         'sdwc-settings',
         'sdwc_settings_page_html'
@@ -36,17 +36,17 @@ function sdwc_settings_page_html() {
     $keys = get_option('sdwc_recaptcha_keys', array('site_key' => '', 'secret_key' => ''));
     ?>
     <div class="wrap">
-        <h1><?php echo esc_html__('WC Review Captcha Settings', 'spam-defender-wooreview-captcha'); ?></h1>
+        <h1><?php echo esc_html__('WC Review Captcha Settings', 'spam-defender-review-captcha-for-woocommerce'); ?></h1>
         <form method="post" action="options.php">
             <?php settings_fields('sdwc_settings_group'); ?>
             <?php do_settings_sections('sdwc-settings'); ?>
             <table class="form-table">
                 <tr valign="top">
-                    <th scope="row"><?php esc_html_e('Site Key', 'spam-defender-wooreview-captcha'); ?></th>
+                    <th scope="row"><?php esc_html_e('Site Key', 'spam-defender-review-captcha-for-woocommerce'); ?></th>
                     <td><input type="text" name="sdwc_recaptcha_keys[site_key]" value="<?php echo esc_attr($keys['site_key']); ?>" size="50"/></td>
                 </tr>
                 <tr valign="top">
-                    <th scope="row"><?php esc_html_e('Secret Key', 'spam-defender-wooreview-captcha'); ?></th>
+                    <th scope="row"><?php esc_html_e('Secret Key', 'spam-defender-review-captcha-for-woocommerce'); ?></th>
                     <td><input type="text" name="sdwc_recaptcha_keys[secret_key]" value="<?php echo esc_attr($keys['secret_key']); ?>" size="50"/></td>
                 </tr>
             </table>
